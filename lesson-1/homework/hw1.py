effe0da4e5556dfce22596a33feb7580803e0e5e
Rustam-0329 <rustam.gulyamov29@gmail.com>
2025-09-07 17:03:59 +0500

    Update and rename hw1.ipynb to hw1.py

---
+++ lesson-1/homework/hw1.py
@@ -0,0 +1,46 @@
+# Lesson-1 | Homework
+
+# 1.Given a side of square. Find its perimeter and area.
+
+a = float(input('Enter the side of square '))
+
+P = 4 * a
+S = a * a
+
+print('Peremeter = ', P)
+print('Square ', S)
+
+
+# 2.Given diameter of circle. Find its length.
+
+l = float(input('Enter the diameter of circle'))
+
+C = 3.14 * l
+
+print('Length = ', C)
+
+
+# 3.Given two numbers a and b. Find their mean.
+
+a = float(input('Enter the number a'))
+b = float(input('Enter the number b'))
+
+mean = (a + b)/2
+
+print('Mean = ', mean)
+
+
+# 4.Given two numbers a and b. Find their sum, product and square of each number.
+
+a = float(input('Enter the number a'))
+b = float(input('Enter the number b'))
+
+sum = a + b
+product = a * b
+square_a = a * a
+square_b = b * b
+
+print('Sum = ', sum)
+print('Product = ', product)
+print('Square a = ', square_a)
+print('Square b = ', square_b)
